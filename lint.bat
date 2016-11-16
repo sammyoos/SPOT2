c445e3dfae82fcfaf462c328075e63a05172a4b7
Oosterhuis, Samuel J (Sam) <samuel.oosterhuis@dematic.com>
2016-11-16 18:47:51 -0500

    Remove extra files

--- lint.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo .
-echo Bootstrap Lint
-call bootlint index.html parser.html
-echo .
-echo JSHint
-call jshint --verbose scripts\parser.js
--- q.bat
+++
@@ -1,1 +0,0 @@
-start D:\spe\tools\tinyweb\tiny d:\spe\repos\SPOT2\ 9000
