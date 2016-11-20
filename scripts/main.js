be1b28ca614c1bdbd56abde9c7e86f4e47e1d812
sammyoos <sam.oosterhuis@dematic.com>
2016-11-19 23:05:35 -0500

    effects now listing, added sellecting for both ing and eff

--- scripts/main.js
+++ scripts/main.js
@@ -14,24 +14,24 @@
 	//
 	
 	spot_ns.iOptions = {
-		"selector" 	: "span.tIngs",
-		"selected" 	: "span.tIngs.label-warning",
-		"selClass" 	: "tIngs label label-warning",
-		"notClass" 	: "tIngs",
+		"selector" 	: "p.tIngs",
+		"selected" 	: "p.tIngs.tag-primary",
+		"selClass" 	: "tIngs tag tag-primary",
+		"notClass" 	: "tIngs tag",
 		"global"	 	: getIdx().ingL,
 		"list"			: "#ingredient-list",
-		// "fpSelect"	: selectIngredient,
+		"fpSelect"	: selectIngredient,
 	 	"type"			: "i",
 	};
 
 	spot_ns.eOptions = {
-		"selector"  : "span.tEffs",
-		"selected"  : "span.tEffs.label-info",
-		"selClass"  : "tEffs label label-info",
-		"notClass"  : "tEffs",
+		"selector"  : "p.tEffs",
+		"selected"  : "p.tEffs.tag-success",
+		"selClass"  : "tEffs tag tag-success",
+		"notClass"  : "tEffs tag",
 		"global"	  : getIdx().effL,
-		"list"			: "#effects-list",
-		// "fpSelect"	: selectEffect,
+		"list"			: "#effect-list",
+		"fpSelect"	: selectEffect,
 	 	"type"			: "e",
 	};
 
@@ -42,6 +42,20 @@
 	// needs to be reset _every_ pass through...
 	var netIndex = null, validEff = null, validIng = null, ingScopeFilter = null, effScopeFilter = null; 
 
+	function selectIngredient()
+	{
+		$(this).toggleClass( "tag-primary" );
+		redraw();
+		return( false );
+	}
+
+	function selectEffect()
+	{
+		$(this).toggleClass( "tag-success" );
+		redraw();
+		return( false );
+	}
+
 	spot_ns.display_list = function( validList, selected, options )
 	{
 		$( options.list ).empty();
@@ -50,11 +64,12 @@
 		{
 			var item = options.global[ validList[ i ]];
 
-			$( options.list ).append( "<p class=\"tag " 
-					+ "tag-default" 
+			$( options.list ).append( "<p class=\""
+					+ options.notClass
 					+ "\">" + item.nam + "</p>" );
 		}
 
+		$( options.selector ).click( options.fpSelect );
 	}
 
 }( window.spot_ns = window.spot_ns || {}, jQuery ));
@@ -65,8 +80,11 @@ $(document).ready( function()
 
 	spot_ns.gi = new Object();
 	for( i=0; i<spot_ns.iOptions.global.length; i++ ) spot_ns.gi[i]=i;
-	//spot_ns.display_list( [ 0, 1, 2, 3 ] , null, spot_ns.iOptions );
 	spot_ns.display_list( spot_ns.gi, null, spot_ns.iOptions );
+
+	spot_ns.ge = new Object();
+	for( i=0; i<spot_ns.eOptions.global.length; i++ ) spot_ns.ge[i]=i;
+	spot_ns.display_list( spot_ns.ge, null, spot_ns.eOptions );
 
 });
 
