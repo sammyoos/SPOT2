7bd82dc6bf247b62444689c288d1db1367d2dfc1
sammyoos <sam.oosterhuis@dematic.com>
2016-11-28 17:12:54 -0500

    index-base.js now generating again

--- scripts/parser.js
+++ scripts/parser.js
@@ -6,7 +6,7 @@
     var eList = effects.l;
     var effIdx = effects.h[ effNam ];
 
-    if( eList == undefined || eList == null ) debugger;
+    // if( eList == undefined || eList == null ) debugger;
 
     if( eList[ effIdx ].n === "" ) {
       eList[ effIdx ].x = effIdx;
@@ -19,7 +19,7 @@
   }
 
   function parseEffInIng(idx,td,ing){
-    var eff = { x: -1, n: "", v: 1, m: 1 };
+    var eff = { x: -1, v: 1, m: 1 };
 
     var tmpNam = "";
     var aCntr = 0;
@@ -32,7 +32,6 @@
         case 1:
           if( tmpNam.length == "" ) tmpNam = $(this).text().trim(); 
           break; // usually initial image
-        case 1: tmpNam = $(this).text().trim(); break;
         default: // absorb all the rest
           if ($(this).attr("title") === "Magnitude") { eff.m = size.pop(); }
           if ($(this).attr("title") === "Value") { eff.v = size.pop(); }
@@ -44,8 +43,9 @@
     return (eff);
   }
 
-  function parseIngTD(index, rowTop, tdCnt, rowContent, props ) {
+  function parseIngTD(index, rowTop, tdCnt, rowContent, propIdx ) {
     var qText = $(rowContent).text().trim();
+    var props = ( propIdx < 0 )? null : index.i.l[propIdx];
 
     if (rowTop) {
       switch (tdCnt) {
@@ -57,10 +57,11 @@
             var aText = $(this).text().trim();
             switch (aFlds++) {
               case 0:
-                var ingIdx = index.i.h[ aText ];
-                props = index.i.l[ ingIdx ];
+                propIdx = index.i.h[ aText ];
+                props = index.i.l[ propIdx ];
+                // if( props == null ) debugger;
                 props.n = aText;
-                debugger;
+                props.e = [];
                 break;
               case 1: // origin DLC
                   if(aText=="xx"){  
@@ -77,7 +78,7 @@
           break;
 
         case 2:
-          if( props == null ) debugger;
+          // if( props == null ) debugger;
           props.r = qText; // in what region is this found
           break;
 
@@ -88,7 +89,7 @@
         case 0: /* fall through */
         case 1: /* fall through */
         case 2: /* fall through */
-        case 3: props.e.push(parseEffInIng(index,rowContent,props.idx)); break;
+        case 3: props.e.push( parseEffInIng( index, rowContent, props.idx )); break;
         case 4: props.v = qText; break; // value
         case 5: props.w = qText; break; // weight
         case 6: props.q = qText; break; // quantity of merchants selling this ingredient
@@ -96,17 +97,7 @@
       }
     }
     
-    return props;
-  }
-
-  function incMetric(metric,a,b,c){
-    if(!metric.hasOwnProperty(a)){metric[a]={};}
-    if(!metric[a].hasOwnProperty(b)){metric[a][b]={};}
-    if(!metric[a][b].hasOwnProperty(c)){
-      metric[a][b][c]=1;
-    }else{
-      ++metric[a][b][c];
-    }
+    return propIdx;
   }
 
   function parseIngFile(index, fn) {
@@ -119,8 +110,7 @@
             // source: http://stackoverflow.com/questions/15150264/jquery-how-to-stop-auto-load-imges-when-parsehtml
             var new_doc = doc.replace(/<img [^>]*src=['"]([^'"]+)[^>]*>/gi, function (match, capture) { return "<img no_load_src=\"" + capture + "\" />"; });
             var topHalf = true;
-            var ingIdx = 0;
-            var props = null;
+            var propsIdx = -1;
             //var skipper = 0;
 
             $("tr", new_doc).each(function () {
@@ -128,7 +118,7 @@
 
               $(this).find("td").each(function () {
                 //if (skipper++ > 200)return; // TODO: remove this line
-                props = parseIngTD(index,topHalf, iterField++, this, props );
+                propsIdx = parseIngTD(index,topHalf, iterField++, this, propsIdx );
               });
 
               if(iterField === 0){return;} // no fields processed, must be a header row
@@ -138,9 +128,8 @@
               } else {
                 // props.idx = index.ing.num[props.nam];
                 // index.ing.lab[props.idx] = props;
-                incMetric(index.metrics, "ing", "dlc", props.o );
-                incMetric(index.metrics, "ing", "qty", props.q );
                 topHalf = true;
+                propsIdx = -1;
               }
             });
           },
@@ -149,7 +138,7 @@
     );
   }
 
-  function setIngEffIdx( idx, template, hash ) {
+  function betIngEffIdx( idx, template, hash ) {
     idx.h = hash;
     idx.z = Object.keys( idx.h ).length;
     idx.a = new Array( idx.z );
@@ -171,6 +160,37 @@
     return idx;
   }
     
+  function setIngEffIdx( idx, template, hash ) {
+    idx.h = hash;
+    var z = Object.keys( idx.h ).length;
+    idx.z = z;
+
+    idx.a = new Array( z );
+    idx.b = new Array( z );
+    idx.d = new Array( z );
+    idx.l = new Array( z );
+    idx.p = new Array( z );
+    idx.s = new Array( z );
+
+    for( var i=0; i<z; i++ ) {
+      var elem = {}; // clone the template
+      for( var p in template ) {
+        if( template.hasOwnProperty( p ) ) {
+          elem[p] = template[p];
+        }
+      }
+
+      idx.a[i] = true;
+      idx.b[i] = false;
+      idx.d[i] = null;
+      idx.l[i] = elem;
+      idx.p[i] = [];
+      idx.s[i] = false;
+    }
+
+    return idx;
+  }
+    
 
 
   $(document).ready(function () {
@@ -178,49 +198,27 @@
 
     index.i = {};
     setIngEffIdx( index.i, 
-      // { x: 0, n:"", v: -1, w: -1, r: "", o: "", q: "", e: [] },
-      { n:"", v: -1, w: -1, r: "", o: "", q: "", e: [] },
+      { n:"", v: -1, w: -1, r: "", o: "", q: "", e: null },
       { "Abecean Longfin": 0, "Ancestor Moth Wing": 1, "Ash Creep Cluster": 2, "Ash Hopper Jelly": 3, "Ashen Grass Pod": 4, "Bear Claws": 5, "Bee": 6, "Beehive Husk": 7, "Berit's Ashes": 8, "Bleeding Crown": 9, "Blisterwort": 10, "Blue Butterfly Wing": 11, "Blue Dartwing": 12, "Blue Mountain Flower": 13, "Boar Tusk": 14, "Bone Meal": 15, "Briar Heart": 16, "Burnt Spriggan Wood": 17, "Butterfly Wing": 18, "Canis Root": 19, "Charred Skeever Hide": 20, "Chaurus Eggs": 21, "Chaurus Hunter Antennae": 22, "Chicken's Egg": 23, "Creep Cluster": 24, "Crimson Nirnroot": 25, "Cyrodilic Spadetail": 26, "Daedra Heart": 27, "Deathbell": 28, "Dragon's Tongue": 29, "Dwarven Oil": 30, "Ectoplasm": 31, "Elves Ear": 32, "Emperor Parasol Moss": 33, "Eye of Sabre Cat": 34, "Falmer Ear": 35, "Farengar's Frost Salt": 36, "Felsaad Tern Feathers": 37, "Fine-Cut Void Salts": 38, "Fire Salts": 39, "Fly Amanita": 40, "Frost Mirriam": 41, "Frost Salts": 42, "Garlic": 43, "Giant's Toe": 44, "Giant Lichen": 45, "Gleamblossom": 46, "Glow Dust": 47, "Glowing Mushroom": 48, "Grass Pod": 49, "Hagraven Claw": 50, "Hagraven Feathers": 51, "Hanging Moss": 52, "Hawk's Egg": 53, "Hawk Beak": 54, "Hawk Feathers": 55, "Histcarp": 56, "Honeycomb": 57, "Human Flesh": 58, "Human Heart": 59, "Ice Wraith Teeth": 60, "Imp Stool": 61, "Jarrin Root": 62, "Jazbay Grapes": 63, "Juniper Berries": 64, "Large Antlers": 65, "Lavender": 66, "Luna Moth Wing": 67, "Moon Sugar": 68, "Mora Tapinella": 69, "Mudcrab Chitin": 70, "Namira's Rot": 71, "Netch Jelly": 72, "Nightshade": 73, "Nirnroot": 74, "Nordic Barnacle": 75, "Orange Dartwing": 76, "Pearl": 77, "Pine Thrush Egg": 78, "Poison Bloom": 79, "Powdered Mammoth Tusk": 80, "Purple Mountain Flower": 81, "Red Mountain Flower": 82, "River Betty": 83, "Rock Warbler Egg": 84, "Sabre Cat Tooth": 85, "Salmon Roe": 86, "Salt Pile": 87, "Scaly Pholiota": 88, "Scathecraw": 89, "Silverside Perch": 90, "Skeever Tail": 91, "Slaughterfish Egg": 92, "Slaughterfish Scales": 93, "Small Antlers": 94, "Small Pearl": 95, "Snowberries": 96, "Spawn Ash": 97, "Spider Egg": 98, "Spriggan Sap": 99, "Swamp Fungal Pod": 100, "Taproot": 101, "Thistle Branch": 102, "Torchbug Thorax": 103, "Trama Root": 104, "Troll Fat": 105, "Tundra Cotton": 106, "Vampire Dust": 107, "Void Salts": 108, "Wheat": 109, "White Cap": 110, "Wisp Wrappings": 111, "Yellow Mountain Flower": 112 } 
     );
 
     index.e = {};
     setIngEffIdx( index.e, 
-      // { x: 0, n: "", f: false, i: [] },
-      { n: "", f: false, i: [] },
+      { n: "", f: false },
       { "Cure Disease": 0, "Damage Health": 1, "Damage Magicka Regen": 2, "Damage Magicka": 3, "Damage Stamina Regen": 4, "Damage Stamina": 5, "Fear": 6, "Fortify Alteration": 7, "Fortify Barter": 8, "Fortify Block": 9, "Fortify Carry Weight": 10, "Fortify Conjuration": 11, "Fortify Destruction": 12, "Fortify Enchanting": 13, "Fortify Health": 14, "Fortify Heavy Armor": 15, "Fortify Illusion": 16, "Fortify Light Armor": 17, "Fortify Lockpicking": 18, "Fortify Magicka": 19, "Fortify Marksman": 20, "Fortify One-handed": 21, "Fortify Pickpocket": 22, "Fortify Restoration": 23, "Fortify Smithing": 24, "Fortify Sneak": 25, "Fortify Stamina": 26, "Fortify Two-handed": 27, "Frenzy": 28, "Invisibility": 29, "Lingering Damage Health": 30, "Lingering Damage Magicka": 31, "Lingering Damage Stamina": 32, "Paralysis": 33, "Ravage Health": 34, "Ravage Magicka": 35, "Ravage Stamina": 36, "Regenerate Health": 37, "Regenerate Magicka": 38, "Regenerate Stamina": 39, "Resist Fire": 40, "Resist Frost": 41, "Resist Magic": 42, "Resist Poison": 43, "Resist Shock": 44, "Restore Health": 45, "Restore Magicka": 46, "Restore Stamina": 47, "Slow": 48, "Waterbreathing": 49, "Weakness to Fire": 50, "Weakness to Frost": 51, "Weakness to Magic": 52, "Weakness to Poison": 53, "Weakness to Shock": 54 } 
     );
-
-    index.p = {};
-    index.p.z = 0;
-    index.p.l = [];
-    index.p.i = [ null, null, [], [] ];
-    index.p.e = [ null, [], [], [], [], [], [], [] ];
-
-    index.metrics = {};
 
     // fullJSON.text( JSON.stringify( index ));
     // source: http://stackoverflow.com/questions/3709597/wait-until-all-jquery-ajax-requests-are-done
     $.when(
         parseIngFile(index, "source_data/simple_www.uesp.net_wiki_Skyrim_Ingredients.html")
     ).done(function (p1) {
-      /*
-      console.info( 'parsing complete' );
-      indexData( index.ing );
-      console.info( 'index of ings' );
-      indexData( index.eff );
-      console.info( 'index of effs' );
-      buildPotions(index);
-      console.info( 'potion building' );
-      */
-      $("#fullJSON").text("/* autogenerated content -- DO NOT MODIFY\n" +
-          " * to change this data you must:\n" +
-          " * 1. edit scripts/parser.js\n" +
-          " * 2. load parser.html\n" +
-          " * 3. paste the content into scripts/index.js\n" +
-          " */\n\n" +
-          "function getIdx() { return(" +
-          JSON.stringify(index, null, ' ') +
-          "\n);}");
+      $("#fullJSON").text(
+            "// autogenerated content -- DO NOT MODIFY\n"
+          + "(function( spot_ns, \$, undefined ) {\n"
+          + "spot_ns.index = \n"
+          + JSON.stringify( index, null, ' ')
+          + ";\n}( window.spot_ns = window.spot_ns || {}, jQuery ));\n" );
     });
   });
 }());
