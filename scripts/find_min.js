18e1ad28e638b5d69c5c0268c3e9ac1485bc6380
sammyoos <sam.oosterhuis@dematic.com>
2016-12-18 09:45:13 -0500

    broken

--- scripts/find_min.js
+++ scripts/find_min.js
@@ -5,38 +5,38 @@
 ( function( spot_ns, $, undefined ) {
   "use strict";
 
-  spot_ns.qGetScript = function( scripts ) 
-  {
-    let script = scripts.shift();
-    let path = '/scripts/' + script + '.js';
-    console.log( 'find_min: Loading ' + path );
+  spot_ns.qGetScript = function( script ) {
+    return new Promise(
+      function( resolve, reject )
+      {
+        let path = '/scripts/' + script + '.js';
+        console.log( 'find_min: Loading ' + path );
 
-    $.getScript( path )
-      .done( function( script, textStatus ) {
-        console.log( 'find_min: Loaded' );
-
-        if( scripts.length === 0 ) { 
-          // spot_ns.JSON_dump( '', spot_ns.index );
-        } else {
-          qGetScripts( scripts );
-        }
-      })
-      .fail( function( jqxhr, settings, exception ) {
-        console.error( 'find_min: Failed' );
-        console.log( jqxhr.status );
-        console.log( jqxhr );
-        console.log( settings );
-        console.log( exception );
-      });
-  }
+        $.getScript( path )
+          .done( function( script, textStatus ) {
+            console.log( 'find_min: Loaded' );
+            resolve();
+            spot_ns[ script ]();
+          })
+          .fail( function( jqxhr, settings, exception ) {
+            console.error( 'find_min: Failed' );
+            console.error( jqxhr.status );
+            console.error( jqxhr );
+            console.error( settings );
+            console.error( exception );
+            reject( exception );
+          });
+      }
 
 }( window.spot_ns = window.spot_ns || {}, jQuery ));
 
 
 $(document).ready(function () {
+  console.info( 'document is now ready' );
   jQuery.ajaxSetup({ cache: false });
 
-  spot_ns.qGetScript( [ 'parser' ] );
+  // spot_ns.qGetScript( [ 'parser' ] );
+  spot_ns.qGetScript( 'fake_parser' );
   spot_ns.parser();
   console.log( spot_ns.index );
   spot_ns.JSON_dump( '', spot_ns.index );
