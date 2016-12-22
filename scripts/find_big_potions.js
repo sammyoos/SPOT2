e776d2aa3f58398fc61f00e47b02e9b407a42e80
sammyoos <sammyo@gmail.com>
2016-12-22 16:29:21 -0500

    24% performance increase for big pot selection

--- scripts/find_big_potions.js
+++ scripts/find_big_potions.js
@@ -11,51 +11,48 @@ spot_ns.buildBigPotions = function(idx)
   var seen = {};
 
   for( var i=0; i<max-1; i++ ){
-    var A = pList[i];
+    const A = pList[i], Ai = A.i, Ae = A.e;
 
     for( var j=i+1; j<max; j++ ){
-      var B = pList[j];
+      var B = pList[j], Bi = B.i, Be = B.e;
 
       // at least one ingredient must be in common to proceed
-      if( A.i[0] != B.i[0] && A.i[0] != B.i[1] && A.i[1] != B.i[0] && A.i[1] != B.i[1] ) continue;
+      if( Ai[0] != Bi[0] && Ai[0] != Bi[1] && Ai[1] != Bi[0] && Ai[1] != Bi[1] ) continue;
 
       // if an other variant of this potion has already been seen, jump out...
-      var ings = spot_ns.join( A.i, B.i );
+      var ings = spot_ns.join( Ai, Bi );
       var key = ings.join(':');
-      if( key in seen ) continue;
+      if( key in seen ) { continue; }
       seen[key] = true; // from now on this key has been seen
 
       // if no new effects are generated, this new potion is viable but useless...
-      var mustHit = Math.max(A.e.length,B.e.length);
-      var effs = spot_ns.join( A.e, B.e );
+      var mustHit = Math.max(Ae.length,Be.length);
+      var effs = spot_ns.join( Ae, Be );
       var effLen = effs.length;
       if( effLen <= mustHit ) {
         ++idx.m.u[3][effLen];
         continue;
       }
 
-      if( idx.p.z%100 == 0 ) {
-        console.info( "[" + idx.p.z + "] Adding... " + key );
-        // if( idx.p.z > 35000 ) debugger;
-      }
-      // if( idx.p.z%100 == 0 ) fj.append( "<br>[" + idx.p.z + "] Adding... " + key );
+      if( idx.p.z%1000 == 0 ) { console.info( "[" + idx.p.z + "] Adding... " + key ); }
 
+      const position = idx.p.z++;
       var pot = {
-        x: idx.p.z++,
+        x: position,
         i: ings,
         e: effs
       };
 
       pList.push( pot );
-      idx.p.i[ 3        ].push( pot.x );
-      idx.p.e[ effLen   ].push( pot.x );
-      idx.i.p[ pot.i[0] ].push( pot.x );
-      idx.i.p[ pot.i[1] ].push( pot.x );
-      idx.i.p[ pot.i[2] ].push( pot.x );
+      idx.p.i[ 3        ].push( position );
+      idx.p.e[ effLen   ].push( position );
+      idx.i.p[ pot.i[0] ].push( position );
+      idx.i.p[ pot.i[1] ].push( position );
+      idx.i.p[ pot.i[2] ].push( position );
 
       var effPos = 0, effNeg = 0;
       for( var i=0; i<effLen; i++ ) {
-        idx.e.p[ pot.e[i] ].push( pot.x );
+        idx.e.p[ pot.e[i] ].push( position );
         if( idx.e.l[i].f ) {
           ++effPos;
         } else {
--- scripts/utils.js
+++ scripts/utils.js
@@ -65,7 +65,7 @@
 	}
 
 	// function: join
-	spot_ns.join = function(a, b)
+	spot_ns.join_old = function(a, b)
 	{
 		// if( a == null || b == null ) debugger;
 		var aLen = a.length, bLen = b.length;
@@ -77,6 +77,32 @@
 			if      (a[ai] < b[bi] ){ result[idx++] = a[ai++]; }
 			else if (a[ai] > b[bi] ){ result[idx++] = b[bi++]; }
 			else { result[idx++] = a[ai++]; bi++; }
+		}
+
+		while( ai < aLen ) { result[idx++] = a[ai++]; }
+		while( bi < bLen ) { result[idx++] = b[bi++]; }
+
+		result.length = idx;
+
+		return result;
+	}
+
+	// function: join
+	spot_ns.join = function(a, b)
+	{
+		// if( a == null || b == null ) debugger;
+		const aLen = a.length, bLen = b.length;
+		var ai=0, bi=0, result = new Array( aLen + bLen );
+		var idx = 0;
+
+		while( ai < aLen && bi < bLen )
+		{
+			const A = a[ai];
+			const B = b[bi];
+
+			if      (A < B ){ result[idx++] = A; ai++; }
+			else if (A > B ){ result[idx++] = B; bi++; }
+			else { result[idx++] = A; ai++; bi++; }
 		}
 
 		while( ai < aLen ) { result[idx++] = a[ai++]; }
